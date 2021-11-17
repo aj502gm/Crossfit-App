4233198cd5b74ba8544a5dd3124a3957027f2c43
aj502gm <20004118@galileo.edu>
2021-11-17 12:33:33 -0600

    project restructuring

---
+++ App/js/app.js
@@ -0,0 +1,23 @@
+function buton(){
+    window.location.href = "../views/registro.html"
+}
+
+function buton1(){
+    window.location.href = "../views/registro.html"
+}
+
+
+function buton_horatios(){
+    window.location.href = "../views/horarios.html"
+}
+
+
+
+function buton_leader(){
+    window.location.href = "../views/leaderborad.html"
+}
+
+
+function buton_entren(){
+    window.location.href = "../views/entrenadores.html"
+}
